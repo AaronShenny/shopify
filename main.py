a2b124247dfd98f34cdc3e71f00aa81c116e9507
AaronShenny <aaronshenny102.go@gmail.com>
2023-11-20 20:12:24 +0530

    added funtion create_user()

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-#program
+import getpass
+database = {
+    'user' : {
+        'aaronshenny':{
+            'name' : 'Aaron Shenny',
+            'password' : '123'
+        },
+        'user':{
+            name : 'user',
+            password :'root'
+        }
+    }
+}
+
+
+
+
+
+def create_user():
+    print('[Sorry, Dur to the limited knowlegde, Now creating account will be deleted after the program closes. Use the default username and password]')
+    print('Creating a user account..')
+    name = input('Enter your username : ')
+    try: 
+        password = getpass.getpass()
+    except Exception as Error:
+        print('Error : ', Error)
+    try:
+        database['user'][name] = {
+            'name': name,
+            'password': password
+        }
+    except Exception as Error:
+        print('Error : ', Error)
+print(database)
+create_user()
+print(database)
