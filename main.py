e677923f76757efcae30dba139e99615018e1599
AaronShenny <aaronshenny102.go@gmail.com>
2023-11-21 18:34:23 +0530

    editing

--- main.py
+++ main.py
@@ -13,12 +13,15 @@ database = {
         
         }
     },
+    'vegetables':{
+        
+    }
     
 }
-def create_user():
+def create_user(name):
     print('[Sorry, Dur to the limited knowlegde, Now creating account will be deleted after the program closes. Use the default username and password]')
     print('Creating a user account..')
-    name = input('Enter your username : ')
+    #name = input('Enter your username : ')
     if name in database['user']:
         print('Same user has been found in our databse. Please login ...')
     else:
@@ -37,23 +40,27 @@ def create_user():
 
 def sign_in():
     username = input('Enter your username : ')
-    try:
-        if username in database['user']:
-            password1 = getpass.getpass()
-            try:
-                if password1 == database['user'][username]['password']:
-                    print('Account logined..')
-                    print(f'Welcome {username}')
-            except:
-                print('Incorrect password')
-            print('found')
-    except:
+    if username in database['user']:
+        password1 = getpass.getpass()   
+        if password1 == database['user'][username]['password']:
+            print('Account logined..')
+            print(f'Welcome {username}')
+        else:
+            print('Incorrect Password...')
+    else:  
         print('Account not Found')
-    
+        print('Create an account...')
+        create_user(username)
+
+
+login = False
 
 while True:
     ch = int(input('Enter the choice'))
     if ch == 1:
         sign_in()
+        break
     elif ch == 2:
-        create_user()
+        name = input('Enter your username : ')
+        create_user(name)
+        
