7ca24e5351f64744d4f248efa1fc51f1b2f9b273
AaronShenny <aaronshenny102.go@gmail.com>
2023-12-28 15:39:38 +0530

    fixe

--- main.py
+++ main.py
@@ -239,7 +239,7 @@ def recipt(username):
     print('='*55)
     time2 = time.asctime()
 
-    print('Name : ',username,'\t\t\t','Date : ',time2)
+    print('Name : ',username,'\t\t','Date : ',time2)
     for i in  brougth_items:
         for j in i:
             print(j,end=' ')
