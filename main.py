b81c903cd41a7117841ddde12480f81844e5853c
AaronShenny <aaronshenny102.go@gmail.com>
2023-12-29 12:37:23 +0530

    table trial

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
-import getpass   #Prompt the user for a password without echoing
-import time      #This module provides various time-related functions
+''' 
+    This program is developed by a group of the 5 students.
+    This program is a vegetable & fruit store management system. 
+    It allows users to create an account, sign in, buy 
+    vegetables, and view their receipts
+'''
+# Import necessary modules
+import getpass  # Module to input passwords without echoing
+import time  # Module for time-related functions
 
-user_buy =  {}   #Intializing a variable
+# Initialize variables
+user_buy = {}  # Stores user purchases
 
+# The database containing user information, vegetables, and fruits
 database = {     #The Whole Database . 
     'user' : {
         'aaronshenny':{
             'name' : 'Aaron Shenny',
             'password' : '123'
         },                                            
-        'user':{                                      #userdata
+        'user':{                         #Default user
 
             'name' : 'Guest',
             'password' :'root'
         },                  
         'aswinaravind27':{
-            'name' : 'Aswin Aravind',
+            'name' : 'Aswin Aravind',    #User database
             'password':'aswi'
 
           
@@ -23,64 +32,64 @@ database = {     #The Whole Database .
     },
     'vegetables':{
         'tomato' : {
-            'name':'Tomato',
-            'price':48,
-                'stock':10
+            'name' : 'Tomato',
+            'price' : '48RS',
+                'stock' : 10            #Vegetable Database
         },
         'onion': {
             'name':'Onion',
-            'price':79,
+            'price':'79RS',
             'stock':15
         },
-        'green chilli':{
+        'greenchilli':{
             'name':'Green chilli',
-            'price':46,
+            'price':'46RS',
             'stock':12
         },
         'beetroot':{
             'name':'Beetroot',
-            'price':34,
-            'stock':14                               #vegetable database
+            'price':'34RS',
+            'stock':14                               
         },
         'potato':{
             'name':'Potato',
-            'price':40,
+            'price':'40RS',
             'stock':16
         },
         'cabbage':{
             'name':'Cabbage',
-            'price':25,
+            'price':'25RS',
             'stock': 13
         },
         'carrot':{
             'name':'Carrot',
-            'price':39,
+            'price':'39RS',
             'stock':17
         
         },
         'corn':{
             'name':'Corn',
-            'price':35,
+            'price':'35RS',
             'stock':19
         },
         'coconut':{
             'name':'Coconut',
-            'price':37,
+            'price':'37RS',
             'stock':16
         },
         'ginger':{
             'name':'Ginger',
-            'price':111,
+            'price':'111RS',
             'stock':20
         },
         'elephant yam':{
             'name':'Elephant Yam',
-            'price':34,
+            'price':'34RS',
             'stock':15
         },
         'brinjal':{
             'name':'Brinjal',
-            'price':33,
+            'price':'33RS',
             'stock':18
 
         }
@@ -89,15 +98,17 @@ database = {     #The Whole Database .
 
     }
 }
-def create_user(name):                   #This Function is used to create and add a user into the database
+
+# Function to create a new user account
+def create_user(name):                   
     print()
     print()
     print('SIGN-UP')
     print('Sorry, Due to the limited knowlegde, Now creating account will be deleted after the program closes. Use the default username and password...')
     print('Creating a user account...')
     username = input('Username : ')
-    if username in database['user']:  #This will check if the user has already created an account
-        print('Same user has been found in our database. Please login ...')
+    if username in database['user']:  #This will check if the user had already created account
+        print('Same user has been found in our databse. Please login ...')
         
     else:
         try: 
@@ -110,59 +121,65 @@ def create_user(name):                   #This Function is used to create and ad
                 'password': password
             }
         except Exception as Error:
-            print('Error : ', Error)                   
-def sign_in():                                           #Function that helps to sign in to their account
-    #login = False
+            print('Error : ', Error)      
+        print('Account created successfully...')
+
+# Function for user sign-in             
+def sign_in():                                          
     while True:
         print()
         print()
         print('\t\t\tLOGIN')
         print()
         username = input('Username : ')
-        if username in database['user']:                 #Checking if the given username is matching with usernames in the database
+        if username in database['user']:                               #Checking given Username is matching with usernames in databse
             password1 = getpass.getpass(prompt = 'Password : ')   
             if password1 == database['user'][username]['password']:    #Checking if the given password is correct with database
                 time.sleep(1)
-                print('Account logined...')
+                print('Account logined..')
+                print()
                 print('Welcome',database['user'][username]['name'])
                 username1 = username
-                login = True  #Intializing the varible as True
-                return username,login #Returning username and login variable 
+                login = True                                           #Intializing the varible as True
+                return username,login                                  #Returning username and login variable 
                 break
             else:
-                login = False #Intializing the varible as True
+                login = False                                          #Intializing the varible as True
                 print('Incorrect Password...')
                 login_checker(login)   
-                return username,login  #Returning username and login variable 
+                return username,login                                  #Returning username and login variable 
         else:  
             time.sleep(1)
             print('Account not Found')
-            time.sleep(1)   #If the account is not found in the database then create_user() is called
+            time.sleep(1)                                              #If the account didnt found on the database then create_user() is called
             print('Creating an account...')
             time.sleep(1)
             name  = input('Full name : ')
             create_user(name)
-       
-def buy(l,username): #Function that helps user to buy products
-    brought_items = []
-    print('Type "0" or "exit" after finishing adding the products')
+
+# Function for purchasing items
+def buy(l,username):                
+    brougth_items = []
     while True:
-        item = input('Enter an item : ').lower() #User enters the product they need
-        if item == 'exit' or item == '0': #To exit the while loop
+        print()
+        item = input('Enter an item : ').lower()                        #User enters the product they need
+        if item == 'exit' or item == '0':                               #Exiting the loop
             break
-        elif item in brought_items:
-            print()                      #Checking the cart if the user has already brought the items
-            print('Item is already in the cart!!!')
+        elif item in brougth_items:
+            print()                                                     #Checking the cart if the user had already brougtj
+            print('Item is already in the cart!!')
             for i in l:
                 if item.title() == i[0]: 
-                    print(f'Product{i[0]}')
+                    print(f'Product : {i[0]}')
                     print(f'Quantity : {i[1]}') 
-            change = input('Do you want to change the quantity ? : [Yes/No] ').lower()  #Asking user if they need to change the quantity
+            print()
+            change = input('Do you want to change the quantity  ? : [yes/no] ')
+            print()                    #Asking the user if they want to change the quantity
             if change == 'yes':
                 for i in l:
                     if item.title() == i[0]:
                         product,quantity = i   #Unpacking the tuple to change
-                        quantity = float(input(f'How much kilo you need for {database["vegetables"][item]["name"]} : ')) #Asking the change in quantity
+                        quantity = float(input(f'How much kilo you need for {database["vegetables"][item]["name"]} : ')) #Asking the change
                         t = product,quantity   #Packing the tuple
                         l.remove(i)            #Removing the existing tuple
                         l.append(t)            #Adding the new tuple into list
@@ -173,77 +190,87 @@ def buy(l,username): #Function that helps user to buy products
                     print('Item is already added')
             else:
                 try:
-                    if item.lower() in database['vegetables']: #Checking the item in database
+                    if item.lower() in database['vegetables']:          #Checking the product is in database
                         qut = float(input(f'How much kilo you need for {database["vegetables"][item]["name"]} : ')) #Asking the quantity
-                        if qut <= 0:
-                            print('The digit should be more than 0')
+                        if qut < 0:
+                            print('The quantity should be more than 0')                    #Checking the quantity is more than 0
                             buy(l,username)
                             break
-                        if qut > database['vegetables'][item]['stock']:
-                            print(f'The digit should be less than the TOTAL STOCK, Remaining Stock : {database["vegetables"][item]["stock"]}')
+                        if qut > database['vegetables'][item]['stock']:                    #Checking the given quantity is less than the stock
+                            print(f'The quantity should be less than the TOTAL STOCK, Remaining Stock : {database["vegetables"][item]["stock"]}')
                             buy(l,username)
                             break
 
-                        brought_items.append(item) 
+                        brougth_items.append(item)                                          #Adding the item into the cart
                         items = (database['vegetables'][item]['name'],qut)
                         l.append(items)
 
                         database['vegetables'][item]['stock'] = database['vegetables'][item]['stock'] - qut     
 
-                        print(f"Remaining Stocks = {database['vegetables'][item]['stock']} kg")
+                        print(f"Remaing Stocks = {database['vegetables'][item]['stock']}")
 
-                        if database['vegetables'][item]['stock'] == 0:
+                        if database['vegetables'][item]['stock'] == 0:                   
                             del database['vegetables'][item]
-                        #stock_reducer(item,qut)
+                        
                     else:
                         print('Item not Found')
-                except ValueError:
-                    print('Please enter a valid value...')
+                except ValueError:                                                    #Exception handling
+                    print('Please enter an valid value...')
    
     user_buy[username] = l
-    print(user_buy)
-    return user_buy
+    
+    return user_buy             
 
-def list1(database):
+#Function for listing the items
+def list1(database):                                                                   
     vegetable_data = database.get('vegetables')
     fruits_data = database.get('fruits')
     if not vegetable_data:
-        print("No vegetable data found!")
+        print("No vegetable data found!")                                                    #Checking if the database is empty or not
         return
     print()
-    print("------------------------------------")
-    print("| Vegetable      | Price | Stock   |")
-    print("------------------------------------")
+    print("------------------------------------\t\t\t ---------------------------------")
+    print("| Vegetable      | Price | Stock   |\t\t\t | Fruits      | Price | Stock   |")
+    print("------------------------------------\t\t\t ---------------------------------")
 
     
     for veg_name, veg_info in vegetable_data.items():
-        name = veg_info.get('name', 'N/A')  
-        price = veg_info.get('price', 'N/A')                
-        stock = veg_info.get('stock', 'N/A')
+        vname = veg_info.get('name', 'N/A')  
+        vprice = veg_info.get('price', 'N/A')                
+        vstock = veg_info.get('stock', 'N/A')
+        for fru_name,fru_info in fruits_data:
+            fname = fru_infro.get('name', 'N/A')  
+            fprice = fru_infro.get('price', 'N/A')                
+            fstock = fru_infro.get('stock', 'N/A')
+            print(f"| {vname.ljust(15)}| {vprice.ljust(6)}| {str(vstock).ljust(8)}|")
 
-        print(f"| {name.ljust(15)}| {price.ljust(6)}| {str(stock).ljust(8)}|")
+    # for fru_name,fru_info in fruits_data:
+    #     fname = fru_infro.get('name', 'N/A')  
+    #     fprice = fru_infro.get('price', 'N/A')                
+    #     fstock = fru_infro.get('stock', 'N/A')
+
+       
 
     print("--------------------------------")
 
-def recipt(username):
-    print('Test Pass 5')
-    confirm =  input('Anything else ? : ').lower()
+def recipt(username):                                                   #Function for printing the recipt
+    confirm =  input('Anything else ..? : ').lower()                    #Asking the user if they want to buy anything else
     if confirm == 'yes':
         l =  user_buy.get(username)
-        buy(l,username)
-    brought_items = user_buy.get(username)
+        buy(l)
+    brougth_items = user_buy.get(username)
+    print()
     print('='*55)
-    print('RECEIPT'.center(50))
+    print('RECIPT'.center(50))
     print('='*55)
-    time2 = time.asctime()
+    time2 = time.asctime()                                              #Getting the current time
 
-    print('Name : ',username,'\t\t\t','Date : ',time2)
-    for i in  brought_items:
+    print('Name : ',username,'\t\t','Date : ',time2)
+    for i in  brougth_items:
         for j in i:
             print(j,end=' ')
         print()
 def login_checker(login):
-    print('Test pass4')
     if login != True:
         sign_in()
 
@@ -264,17 +291,16 @@ def main():
     username = None
     while True:
         time.sleep(1)
-        print('Test pass1')
-
         username,login = sign_in() 
-
-        print('Test pass 2')
         time.sleep(1)
         list1(database)
-        buyaccept =  input('Wanna buy something from our store ??? [Yes/No] : ').lower()
-        if buyaccept == 'yes':
+        print()
+        buyacceot =  input('Wanna buy something from our store ...?? [yes/no] : ').lower()   #Asking the user if they want to buy anything..reconfirming
+        if buyacceot == 'yes':
             time.sleep(1)
             l = []
+            print()
+            print('NOTE : Type  "0" or "exit" after finishing adding the products')
             buy(l,username)
             if user_buy[username] == []:
                 pass
@@ -282,7 +308,7 @@ def main():
                 recipt(username)
         else:
             time.sleep(1)
-            print('Thank you for coming')
+            print('Thank you for comming')
             time.sleep(5)
         break
       
@@ -292,9 +318,9 @@ if __name__ == "__main__":
     main()
     while True:
         time.sleep(2)
-        choice = input("Press 'q' to quit or any other key to continue shopping...: ")
+        choice = input("Press 'q' to quit or any other key to continue shopping...: ")  #Asking the user if they want to continue shopping
         if choice.lower() == 'q':
-            print('Thank you for coming\nVisit again!!!')
+            print('Thank you for coming\nVisit again!!')
             print("Exiting the program...")
             break
         else:
